66ee425651d52a7d20df7734040cfbb19f1f40fa
Sanil2002 <sanilm4637@gmail.com>
2024-09-04 18:39:09 +0530

    comments

--- src/App.js
+++ src/App.js
@@ -4,37 +4,35 @@ import "./styles/styles.scss";
 function App() {
   const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 });
 
-  const eyeLeft = useRef();
+  const eyeLeft = useRef();                                                                   // useRef hooks to create references to DOM elements (left and right eyes)
   const eyeRight = useRef();
 
   const eyeBrowLeft = useRef();
   const eyeBrowRight = useRef();
 
-  function calcAngle(element) {
-    if (!element.current) return;
+  function calcAngle(element) {                                                               // Function to calculate the angle between the element and the mouse pointer
+    if (!element.current) return;                                                             //checking whether the element's ref is available
 
-    let elX = element.current.offsetLeft + element.current.clientWidth / 2;
+    let elX = element.current.offsetLeft + element.current.clientWidth / 2;                   // Get the center coordinates of the element
     let elY = element.current.offsetTop + element.current.clientHeight / 2;
 
-    var rad = Math.atan2(mouseCoordinates.x - elX, mouseCoordinates.y - elY);
-    var rot = rad * (180 / Math.PI) * -1 + -18;
+    var rad = Math.atan2(mouseCoordinates.x - elX, mouseCoordinates.y - elY);                 // Calculate the angle in radians between the element's center and the mouse position and
+    var rot = rad * (180 / Math.PI) * -1 + -18;                                               //... Converting the angle from radians to degrees and adjust the rotation.
 
-    return rot;
+    return rot;                                                                               //return the calculated rotation angle
   }
 
-  const handleMouseMove = (event) => {
-    setMouseCoordinates({ x: event.clientX, y: event.clientY });
+  const handleMouseMove = (event) => {                                                        // Event handler to update mouse coordinates and animate eyebrows
+    setMouseCoordinates({ x: event.clientX, y: event.clientY });                              //updating the state with current mouse cordinates
 
-    eyeBrowLeft.current.style.transform = `translateY(${event.clientY / 50}px)`;
-    eyeBrowRight.current.style.transform = `translateY(${
-      event.clientY / 50
-    }px)`;
+    eyeBrowLeft.current.style.transform = `translateY(${event.clientY / 50}px)`;              // Move the left eyebrow slightly up or down based on the Y position of the mouse
+    eyeBrowRight.current.style.transform = `translateY(${event.clientY / 50}px)`;             // Move the right eyebrow slightly up or down based on the Y position of the mouse
   };
 
-  useEffect(() => {
+  useEffect(() => {                                                                           // useEffect hook to add the mousemove event listener when the component mounts
     document.addEventListener("mousemove", handleMouseMove);
 
-    return () => {
+    return () => {                                                                            //clean-up function
       document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
@@ -47,14 +45,14 @@ function App() {
       </div>
       <div className="eye_container">
         <div
-          ref={eyeLeft}
+          ref={eyeLeft}                                                                       //Left eye element with rotation applied based on mouse position
           style={{
             transform: `rotate(${calcAngle(eyeLeft)}deg)`,
           }}
           className="eye"
         ></div>
         <div
-          ref={eyeRight}
+          ref={eyeRight}                                                                      //Right eye element with rotation applied based on mouse position
           style={{
             transform: `rotate(${calcAngle(eyeRight)}deg)`,
           }}
